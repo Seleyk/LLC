4dcf26649bd0f4efb8a926207123c52b93625023
Steven Seley <105885416+Seleyk@users.noreply.github.com>
2023-02-28 18:38:26 -0600

    Revert "Feature/homepage"

--- client/src/styledComponents/NavbarStyle.jsx
+++
@@ -1,9 +0,0 @@
-import { Box } from "@mui/material";
-import { styled } from "@mui/material";
-
-const NavContainer = styled(Box)({
-    backgroundColor: "#ffffff",
-    boxShadow: "0 1px 10px 0 rgba(0,0,0,.14902)",
-});
-
-export default NavContainer
